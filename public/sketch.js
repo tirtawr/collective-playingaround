84cf18cb4a0ae48edc8a3fda6425a62611e8d186
Schuyler deVos <schuylerdevos@gmail.com>
2020-03-23 14:54:50 -0400

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -2,13 +2,17 @@
 // Open and connect input socket
 let socket = io();
 
+/*
+to-do:
+*/
+
 // Listen for confirmation of connection
 socket.on('connect', function() {
   console.log("Connected", socket.id);
 });
 
 //keep track of all users
-let users = {};
+let players;
 // Is it my turn?
 let myTurn = false;
 //keep track of color
@@ -16,22 +20,40 @@ let myColor;
 //keep track of ink;
 let ink = 0;
 
+let getRandomColor = () => {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function setup() {
+
+  myColor = getRandomColor();
 
   frameRate(30);
   createCanvas(windowWidth, windowHeight);
 
-  // Listen for my turn
-  socket.on('go', function() {
-    myTurn = true;
-    ink = 255;
-  });
+  socket.on('currentPlayer', function({ currentPlayer }){
+    if(currentPlayer === socket.id){
+      console.log(`ITS YOUR TURN DAWG`)
+      myTurn = true;
+      ink = 255;
+    } else {
+      console.log(`Current player: ${currentPlayer}`)
+    }
+  })
 
   // Listen for changes to text
-  socket.on('draw', function(drawData) {
-
+  socket.on('drawPoint', function(drawData) {
     // Update string on screen
     drawLine(drawData);
+  });
+
+  socket.on('allPlayers', function(data) {
+    players = data;
   });
 
 }
@@ -40,16 +62,10 @@ function setup() {
 function drawLine(drawData) {
 
   //then we are drawing the line in the correct color
-  stroke(drawData.color,drawData.data.inkLeft);
+  stroke(drawData.data.color,drawData.data.inkLeft);
   line(drawData.data.x,drawData.data.y,drawData.data.pX,drawData.data.pY);
 }
 
-//mouseDragged function
-//so IF IT'S YOUR TURN AND YOU'VE DRAWN, WE NEED TO EMIT THIS DATA SO EVERYONE ELSE
-//CAN DRAW IT TOO, RIGHT?
-//wait, we just want this to send the coordinates right. we dont actually want it drawn here,
-//we want it drawn through drawline
-//yes, that's what the draw in server does, ok cool
 function mouseDragged() {
   // Ignore if it's not your turn
   if (!myTurn) {
@@ -60,16 +76,17 @@ function mouseDragged() {
     let y = mouseY / height;
     let pX = pmouseX / width;
     let pY = pmouseY / height;
-    socket.emit('draw', {
+    socket.emit('drawPoint', {
       x: x,
       y: y,
       pX: pX,
       pY: pY,
-      inkLeft: ink
+      inkLeft: ink,
+      color: myColor
     });
     if (ink < 1) {
       myTurn = false;
-      socket.emit('next');
+      socket.emit('nextPlayer');
     }
     ink -= 1.5;
   }
@@ -100,17 +117,8 @@ socket.on('allPlayers', function({ players }) {
   console.log(players)
 })
 
-socket.on('gameFinished', function(){
-  console.log('game has finished dawg, go home')
-})
-
-function finishRound(){
-  socket.emit('finishRound')
-}
-
 function setup(){
   createCanvas(800, 600);
   background(0);
-} 
+}
 */
-
