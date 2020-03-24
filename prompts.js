49b265dfcbfbb1b86321ada0e917780790b9d623
Tirta Rachman <tirtawening@gmail.com>
2020-03-23 21:07:51 -0400

    Update prompts

--- prompts.js
+++ prompts.js
@@ -1,10 +1,11 @@
 const Prompts = [
-  'sheep',
-  'bear',
-  'elephant',
-  'pangolin',
-  'clown',
-  'orange'
+  'Draw a hot summer day',
+  'Draw a cold winter night',
+  'Draw Manhattan',
+  'Draw two bats',
+  'Draw a letter',
+  'Draw a horn',
+  'Draw something that frustrates you',
 ]
 
 module.exports = Prompts
