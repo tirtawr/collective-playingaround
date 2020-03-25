22e259789db758b5dd42deae6b130095b6321242
Aditya J <ajain15@uic.edu>
2020-03-25 16:37:47 -0400

    more prompts

--- prompts.js
+++ prompts.js
@@ -6,6 +6,16 @@ const Prompts = [
   'Draw a letter',
   'Draw a horn',
   'Draw something that frustrates you',
+  'Draw the ITP Floor',
+  'Draw Red Burns',
+  'Draw Coronavirus',
+  'Draw Democracy',
+  'Draw Love',
+  'Draw Peace',
+  'Draw Equality',
+  'Draw Art',
+  'Draw Loneliness',
+  'Draw Happiness'
 ]
 
 module.exports = Prompts
